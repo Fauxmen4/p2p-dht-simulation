6ec370b14725a9f447cf04628d3aec0f7d4a785b
Exc0mmun1cad0 <fauxmen4@gmail.com>
2026-04-02 00:44:31 +0300

    static network init in cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,46 @@
 package main
 
 import (
-	"fmt"
 	"my-kad-dht/config"
 	"my-kad-dht/internal/network"
+	"time"
+
+	"go.uber.org/zap"
 )
 
 func main() {
+	// logger
+	log := zap.Must(zap.NewDevelopment())
+
 	// read config
 	cfg := config.LoadConfig("config/example.yaml")
 
 	// init network
 	net := network.New(*cfg)
+	log.Info("create network",
+		zap.Int("Boot nodes", cfg.Network.Bootstrap.NodesCount),
+		zap.Int("Connections", cfg.Network.Bootstrap.Connections_count),
+	)
 
-	fmt.Println(net)
-	_ = net
+	// start network (enable bootstrap)
+	net.StartNetwork()
+	log.Info("network started")
+
+	// nodes join network
+	nodes := net.CreateNNodes(cfg.Network.NodesCount)
+	for _, node := range nodes {
+		net.Join(node)
+
+		time.Sleep(1 * time.Millisecond) // TODO: WTF?
+		go func() {
+			node.Run()
+		}()
+	}
+	log.Info("nodes joined the network",
+		zap.Int("count", cfg.Network.Bootstrap.NodesCount),
+	)
+
+	for _, node := range nodes {
+		node.RoutingTable.Print()
+	}
 }
